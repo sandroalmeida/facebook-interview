8d528587948124898df74f71efefdb0af6ebca5a
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-09-30 10:49:00 -0700

    SlowSum implementation.

---
+++ src/info/sandroalmeida/SlowSum.java
@@ -0,0 +1,38 @@
+package info.sandroalmeida;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class SlowSum {
+
+    public static int getTotalTime(int[] arr) {
+        if(arr.length == 1) return arr[0];
+
+        Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for(int number: arr)
+            maxHeap.add(number);
+
+        int total = 0;
+        while(!maxHeap.isEmpty()){
+            int firstElement = maxHeap.poll();
+            int secondElement = maxHeap.poll();
+            int penalty = firstElement + secondElement;
+            total += penalty;
+            if(maxHeap.isEmpty())
+                break;
+            maxHeap.add(penalty);
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] arr_1 = {4, 2, 1, 3};
+        //int expected_1 = 26;
+        System.out.println(getTotalTime(arr_1));
+
+        int[] arr_2 = {2, 3, 9, 8, 4};
+        //int expected_2 = 88;
+        System.out.println(getTotalTime(arr_2));
+    }
+}
